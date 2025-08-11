1a586c5444f17145e2817da51e25269246b9dbae
fahadiqbal-01 <fahad.iqba.im.47@gmail.com>
2025-08-12 04:48:26 +0600

    lynx

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -28,7 +28,7 @@ const Chat = () => {
           }}
           initial={`hidden`}
           animate={mainControls}
-          transition={{ delay: 0.5, duration: 1.4, ease: easeOut }}
+          transition={{ delay: 0.5, duration:0.7, ease: easeOut }}
           className=" group cursor-pointer overflow-hidden z-50 xl:text-[20px] lg:text-20px md:text-[20px] sm:text-[20px] text-[16px] text-yellow-500 font-GeneralSans bg-black xl:px-[100px] lg:px-[100px] md:px-[100px] sm:px-[80px] px-[60px] py-[10px] border-4 border-black 
          rounded-2xl hover:shadow-2xl hover:drop-shadow-2xl hover:text-black hover:scale-[1.2] hover:border-yellow-500 hover:bg-transparent after:content-[''] after:h-[80px] after:w-[200px] after:bg-yellow-500 after:border-0 after:rounded-[500px]
          after:absolute after:bottom-[-100px] after:left-[50%] after:translate-x-[-50%] after:-z-50 after:duration-500 after:ease-out hover:after:bottom-[-40px] duration-300 ease-out relative "
