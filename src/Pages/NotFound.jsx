417bcf135d642361ec028552d17b5eb90442b2c1
fahadiqbal-01 <fahad.iqba.im.47@gmail.com>
2025-08-12 04:43:32 +0600

    ffffffffffffff

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,10 +1,17 @@
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import React from "react";
 import { Link } from "react-router-dom";
+import { motion } from "motion/react";
 
 export const NotFound = () => {
   return (
-    <div className=" w-full h-auto flex flex-col justify-center items-center ">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{delay:0.3, duration: 0.5, ease: "easeOut" }}
+      viewport={{ once: true }}
+      className=" w-full h-auto flex flex-col justify-center items-center "
+    >
       <p className=" xl:text-[38px] lg:text-[36px] md:text-[32px] text-[28px] font-GeneralSans flex flex-col justify-center items-center pt-[28px] ">
         Lost? <br />
         <span>
@@ -15,6 +22,6 @@ export const NotFound = () => {
         </span>
       </p>
       <DotLottieReact src="json/404.lottie" loop autoplay />
-    </div>
+    </motion.div>
   );
 };
